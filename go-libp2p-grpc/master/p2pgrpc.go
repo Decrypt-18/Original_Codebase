52b3bf156b20810b5b5d4009924874d3e162f705
Decrypt-18 <akshay.deore@mastertech.co.in>
2025-02-17 11:19:42 +0530

    Updated Dependencies

---
+++ go-libp2p-grpc/master/p2pgrpc.go
@@ -0,0 +1,55 @@
+package p2pgrpc
+
+import (
+	"context"
+	// "net"
+
+	host "github.com/libp2p/go-libp2p-host"
+	inet "github.com/libp2p/go-libp2p-net"
+	protocol "github.com/libp2p/go-libp2p-protocol"
+	"google.golang.org/grpc"
+)
+
+// Protocol is the GRPC-over-libp2p protocol.
+const Protocol protocol.ID = "/grpc/0.0.1"
+
+// GRPCProtocol is the GRPC-transported protocol handler.
+type GRPCProtocol struct {
+	ctx        context.Context
+	host       host.Host
+	grpcServer *grpc.Server
+	streamCh   chan inet.Stream
+}
+
+// NewGRPCProtocol attaches the GRPC protocol to a host.
+func NewGRPCProtocol(ctx context.Context, host host.Host, opts ...grpc.ServerOption) *GRPCProtocol {
+	grpcServer := grpc.NewServer(opts...)
+	grpcProtocol := &GRPCProtocol{
+		ctx:        ctx,
+		host:       host,
+		grpcServer: grpcServer,
+		streamCh:   make(chan inet.Stream),
+	}
+	host.SetStreamHandler(Protocol, grpcProtocol.HandleStream)
+	return grpcProtocol
+}
+
+// GetGRPCServer returns the grpc server.
+func (p *GRPCProtocol) GetGRPCServer() *grpc.Server {
+	return p.grpcServer
+}
+
+// Serve initiates the grpc server to accept new connections
+// Note: should be called after registering the server and handlers
+func (p *GRPCProtocol) Serve() error {
+	return p.grpcServer.Serve(newGrpcListener(p))
+}
+
+// HandleStream handles an incoming stream.
+func (p *GRPCProtocol) HandleStream(stream inet.Stream) {
+	select {
+	case <-p.ctx.Done():
+		return
+	case p.streamCh <- stream:
+	}
+}
